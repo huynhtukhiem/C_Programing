95d691b66c224436b35846145e9b473c33b9f60a
Huỳnh Tử Khiêm <huynhtukhiem2006@gmail.com>
2025-09-29 15:18:02 +0700

    Create Bài tập thuần con trỏ (2).c

---
+++ Con trỏ (Pointer)/Bài tập thuần con trỏ (2).c
@@ -0,0 +1,6 @@
+// Dùng hàm không có con trỏ và con trỏ để thấy sự khác biệt của việc dùng con trỏ và không dùng con trỏ
+/*
+Thay đổi giá trị của một biến
+Đổi giá trị của 2 biến
+Tính toán 2 biến
+*/
