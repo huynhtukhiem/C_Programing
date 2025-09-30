c8928e8caa2d7a6dd9719f4242c08c7b1427a0e2
Huỳnh Tử Khiêm <huynhtukhiem2006@gmail.com>
2025-09-30 15:06:16 +0700

    Update and rename Các cách hai báo mảng bằng con trỏ.c to Lý thuyết khai báo mảng bằng con trỏ.md

--- Con trỏ (Pointer)/Pointer + array1D/Các cách hai báo mảng bằng con trỏ.c
+++
@@ -1,8 +0,0 @@
-/*
- Chúng ta có 3 cách chính để khai báo một mảng bằng con trỏ trong C (theo mình tìm hiểu)
-+ Khai báo bằng mảng động (dùng con trỏ)
-+ Khai báo mảng có độ dài biến thiên (dùng con trỏ)
-+ Khai báo mảng động
- => Mỗi cách sẽ có những đặc điểm riêng, phù hợp với từng dạng bài
-Ngoài ra có thể dùng hàm để khai báo mảng nhưng đây chỉ là công cụ thôi công cụ gom code lại cho gọn và tái sử dụng
-*/
